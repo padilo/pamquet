2df1c10371b24ea1e0e70f81e5f0729684940945
Pablo Diaz <padilo@gmail.com>
2022-04-09 23:04:50 +0200

    spinner integrated

--- tui/model.go
+++ tui/model.go
@@ -4,22 +4,28 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	timer timer.Model
+	timer   timer.Model
+	spinner spinner.Model
 }
 
 func NewModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.MiniDot
 	return model{
-		timer: timer.NewWithInterval(10*time.Second, 51*time.Millisecond),
+		timer:   timer.NewWithInterval(10*time.Second, 51*time.Millisecond),
+		spinner: s,
 	}
+
 }
 
 func (m model) Init() tea.Cmd {
-	return m.timer.Init()
+	return tea.Batch(m.timer.Init(), m.spinner.Tick)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -35,6 +41,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, cmd
 	case timer.TimeoutMsg:
 		return m, tea.Quit
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
 	}
 
 	return m, nil
@@ -53,7 +63,8 @@ func (m model) formatTimer() string {
 	min := t.Truncate(time.Minute)
 	sec := t - min
 	ms := t - min - sec.Truncate(time.Second)
-	return fmt.Sprintf("%02dm:%02d:%03d",
+	return fmt.Sprintf("%s %02dm:%02d:%03d",
+		m.spinner.View(),
 		min/time.Minute,
 		sec/time.Second,
 		ms/time.Millisecond,
