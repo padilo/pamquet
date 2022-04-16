bd554767e51cf96e04f3f5aa0bb2e5b8cb71d9ef
Pablo Diaz <padilo@gmail.com>
2022-04-16 16:14:51 +0200

    task select

--- tui/task/model.go
+++ tui/task/model.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/padilo/pomaquet/app/task"
 )
 
 type Model struct {
-	context task.Context
+	context  task.Context
+	selected int
 }
+
+var (
+	styleTask         = lipgloss.NewStyle().Background(lipgloss.Color("0"))
+	styleSelectedTask = lipgloss.NewStyle().Background(lipgloss.Color("5")).Italic(true)
+)
 
 func NewModel() Model {
 	return Model{
-		context: task.Init(),
+		context:  task.Init(),
+		selected: 0,
 	}
 }
 
@@ -27,23 +35,41 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "up":
+			if m.selected > 0 {
+				m.selected--
+			}
+
+		case "down":
+			if m.selected < len(m.context.TaskList)-1 {
+				m.selected++
+			}
 		}
 	}
 	return m, nil
 }
 
 func (m Model) View() string {
-	var s string
-	for _, task := range m.context.TaskList {
+	var taskLines []string
+
+	taskLines = make([]string, len(m.context.TaskList))
+	for i, t := range m.context.TaskList {
 		checked := " "
-		if task.Done {
+		var style lipgloss.Style
+
+		if i == m.selected {
+			style = styleSelectedTask
+		} else {
+			style = styleTask
+		}
+		if t.Done {
 			checked = "x"
+			style = style.Copy().Strikethrough(true)
 		}
 
-		s += fmt.Sprintf("[%s] %s\n", checked, task.Title)
+		taskLines[i] = style.Render(fmt.Sprintf("[%s] %s", checked, t.Title))
+
 	}
 
-	s += "\nPress q to quit.\n"
-
-	return s
+	return lipgloss.JoinVertical(lipgloss.Left, taskLines...)
 }
